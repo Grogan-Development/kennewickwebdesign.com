137ed399b76bead4f3fe2296a5eaee91e4ecfb2b
ZackGrogan <zackariahgrogan@gmail.com>
2026-03-01 15:10:59 -0800

    add static export for render deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   compress: true,
   poweredByHeader: false,
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 768, 1024, 1280, 1536],
+    unoptimized: true,
   },
   experimental: {
     optimizeCss: true,
